9e068f2b37e6f76634e4bd1097d06387a1502223
nady4 <dev@nady4.com>
2026-06-16 23:00:26 -0300

    optimize ram, sweep cache

--- src/config.js
+++ src/config.js
@@ -3,11 +3,17 @@ import { Client, GatewayIntentBits, Partials } from "discord.js";
 export const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.DirectMessages,
   ],
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+  makeCache: (manager) => manager.memoizeData(),
+  sweepers: {
+    messages: {
+      interval: 60,
+      filter: (m) => m.age > 30,
+    },
+  },
 });
