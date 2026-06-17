506958c44e4b83187050809520792565eb29be92
nady4 <dev@nady4.com>
2026-06-16 23:08:30 -0300

    optimize ram, sweep cache

--- src/config.js
+++ src/config.js
@@ -11,6 +11,22 @@ export const client = new Client({
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
   makeCache: (manager) => manager.memoizeData(),
   sweepers: {
+    guilds: {
+      interval: 300,
+      filter: () => true,
+    },
+    channels: {
+      interval: 300,
+      filter: () => true,
+    },
+    roles: {
+      interval: 300,
+      filter: () => true,
+    },
+    users: {
+      interval: 300,
+      filter: () => true,
+    },
     messages: {
       interval: 60,
       filter: (m) => m.age > 30,
